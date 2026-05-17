51ce3864f6e4b9ec8023e8d66ac5aa5878404fd3
minsang <minsang@example.com>
2026-05-17 21:00:34 +0900

    TURN server added

--- public/app.js
+++ public/app.js
@@ -555,7 +555,21 @@ let pendingCallOffer = null;
 
 const callConfig = {
   iceServers: [
-    { urls: "stun:stun.l.google.com:19302" }
+
+    {
+      urls: "stun:stun.l.google.com:19302"
+    },
+
+    {
+      urls: [
+        "turn:openrelay.metered.ca:80",
+        "turn:openrelay.metered.ca:443",
+        "turn:openrelay.metered.ca:443?transport=tcp"
+      ],
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
+
   ]
 };
 
